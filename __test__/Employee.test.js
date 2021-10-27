5111b06bceef7b4a749055f03fb921f011e5daca
minha619 <minha619@gmail.com>
2021-10-26 20:37:11 -0400

    update

---
+++ __test__/Employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require('../lib/Employee')
+
+test ('create an employee object', () => {
+    const employee = new Employee('Amy, 10, email@email.com')
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('gets employeee name', () => {
+    const employee = new Employee('Amy, 10, email@email.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employeee id', () => {
+    const employee = new Employee('Amy, 10, email@email.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// test('gets employeee email', () => {
+//     const employee = new Employee('Amy, 10, email@email.com');
+
+//     expect(employee.getEmail()).toEqual(expect.arrayContaining(employee.email.toString());
+// });
+
+
+test('gets employeee role', () => {
+    const employee = new Employee('Amy, 10, email@email.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
+
